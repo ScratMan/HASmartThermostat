23699991043be0287436bcb58c2ff646ff281e74
fabiannydegger <nydegger.fabian.a@gmail.com>
2021-12-14 09:01:49 +0100

    Disable the reload service.

--- custom_components/smart_thermostat/climate.py
+++ custom_components/smart_thermostat/climate.py
@@ -160,7 +160,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the generic thermostat platform."""
-    await async_setup_reload_service(hass, DOMAIN, PLATFORMS)
+    # await async_setup_reload_service(hass, DOMAIN, PLATFORMS)
 
     platform = entity_platform.current_platform.get()
     assert platform
