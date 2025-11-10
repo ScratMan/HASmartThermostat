ae8ea77a137c0c23a91b8a2c7432828afff502ed
ScratMan <adrien.bonnat@gmail.com>
2025-11-10 20:52:48 +0100

    Ensure HA is running before configuring the listeners.

--- custom_components/smart_thermostat/climate.py
+++ custom_components/smart_thermostat/climate.py
@@ -381,6 +381,22 @@ class SmartThermostat(ClimateEntity, RestoreEntity, ABC):
         """Run when entity about to be added."""
         await super().async_added_to_hass()
 
+        @callback
+        def _async_startup(*_):
+            """Init on startup."""
+            sensor_state = self.hass.states.get(self._sensor_entity_id)
+            if sensor_state and sensor_state.state != STATE_UNKNOWN:
+                self._async_update_temp(sensor_state)
+            if self._ext_sensor_entity_id is not None:
+                ext_sensor_state = self.hass.states.get(self._ext_sensor_entity_id)
+                if ext_sensor_state and ext_sensor_state.state != STATE_UNKNOWN:
+                    self._async_update_ext_temp(ext_sensor_state)
+
+        if self.hass.state == CoreState.running:
+            _async_startup()
+        else:
+            self.hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, _async_startup)
+
         # Add listener
         self.async_on_remove(
             async_track_state_change_event(
@@ -410,22 +426,6 @@ class SmartThermostat(ClimateEntity, RestoreEntity, ABC):
                     self.hass,
                     self._async_control_heating,
                     self._keep_alive))
-
-        @callback
-        def _async_startup(*_):
-            """Init on startup."""
-            sensor_state = self.hass.states.get(self._sensor_entity_id)
-            if sensor_state and sensor_state.state != STATE_UNKNOWN:
-                self._async_update_temp(sensor_state)
-            if self._ext_sensor_entity_id is not None:
-                ext_sensor_state = self.hass.states.get(self._ext_sensor_entity_id)
-                if ext_sensor_state and ext_sensor_state.state != STATE_UNKNOWN:
-                    self._async_update_ext_temp(ext_sensor_state)
-
-        if self.hass.state == CoreState.running:
-            _async_startup()
-        else:
-            self.hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, _async_startup)
 
         # Check If we have an old state
         old_state = await self.async_get_last_state()
